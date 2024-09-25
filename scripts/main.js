8c273680c60e0505bdb2debdebb4df82dfcfefba
Nvntdad <nvntdadhich@gmail.com>
2024-09-25 21:45:45 +0530

    Initial commit

---
+++ scripts/main.js
@@ -0,0 +1,229 @@
+
+const app = document.querySelector("#app");
+const delay = ms => new Promise(res => setTimeout(res, ms));
+    
+    
+app.addEventListener("keypress", async function(event){
+  if(event.key === "Enter"){
+    await delay(150);
+   getInputValue();
+   
+    removeInput();
+    await delay(150);
+    new_line();
+  }
+});
+
+app.addEventListener("click", function(event){
+  const input = document.querySelector("input");
+  input.focus();
+})
+
+
+async function open_terminal(){
+    createText(`
+███╗░░██╗░█████╗░██╗░░░██╗███╗░░██╗███████╗███████╗████████╗  ██████╗░░█████╗░██████╗░██╗░░██╗██╗░█████╗░██╗░░██╗
+████╗░██║██╔══██╗██║░░░██║████╗░██║██╔════╝██╔════╝╚══██╔══╝  ██╔══██╗██╔══██╗██╔══██╗██║░░██║██║██╔══██╗██║░░██║
+██╔██╗██║███████║╚██╗░██╔╝██╔██╗██║█████╗░░█████╗░░░░░██║░░░  ██║░░██║███████║██║░░██║███████║██║██║░░╚═╝███████║
+██║╚████║██╔══██║░╚████╔╝░██║╚████║██╔══╝░░██╔══╝░░░░░██║░░░  ██║░░██║██╔══██║██║░░██║██╔══██║██║██║░░██╗██╔══██║
+██║░╚███║██║░░██║░░╚██╔╝░░██║░╚███║███████╗███████╗░░░██║░░░  ██████╔╝██║░░██║██████╔╝██║░░██║██║╚█████╔╝██║░░██║
+╚═╝░░╚══╝╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░╚══╝╚══════╝╚══════╝░░░╚═╝░░░  ╚═════╝░╚═╝░░╚═╝╚═════╝░╚═╝░░╚═╝╚═╝░╚════╝░╚═╝░░╚═╝`);
+  createText("Welcome ");
+  await delay(700);
+  createText("Starting the server...");
+  await delay(1500);
+  createText("write 'dank' to see the list of the command");
+//   createCode("git", "Github Profile");
+//   createCode("all", "This Commands Will Provide You All the Working Commands");
+//   createCode("contact", "Email For Any Queries");
+//   createCode("clear", "It Clears The Terminal");
+  
+
+  await delay(500);
+  new_line();
+}
+
+
+function new_line(){
+  
+  const p = document.createElement("p");
+  const span1 = document.createElement("span");
+  const span2 = document.createElement("span");
+  p.setAttribute("class", "path")
+  p.textContent = "#Navneet Dadhich's";
+  span1.textContent = "";
+  span2.textContent = " ~/portfolio";
+  p.appendChild(span1);
+  p.appendChild(span2);
+  app.appendChild(p);
+  const div = document.createElement("div");
+  div.setAttribute("class", "type")
+  const i = document.createElement("i");
+  i.setAttribute("class", "fas fa-angle-right icone")
+  const input = document.createElement("input");
+  div.appendChild(i);
+  div.appendChild(input);
+  app.appendChild(div);
+  input.focus();
+  
+}
+
+function removeInput(){
+  const div = document.querySelector(".type");
+  app.removeChild(div);
+}
+
+async function getInputValue(){
+  
+  const value = document.querySelector("input").value;
+  if(value === "dank"){
+    trueValue(value);
+    createCode("git","");
+    createCode("projects","");
+    createCode("contact","");
+    createCode("clear","");
+    createCode("image","");
+    createCode("skills","")
+  }
+  else if(value=== "git"){
+    trueValue(value);
+    createText("Click here For project codes <a href='https://github.com/Nvntdad' target='_blank'>https://github.com/Nvntdad</a>")
+  }
+  else if(value === "projects"){
+    trueValue(value);
+    createText("Here are some of my projects mentioned which i made earlier to the development journey.\n")
+    createText("<a href='https://nvntdad.github.io/friend-s-website/' target='_blank'><i class='fab fa-wrench '></i> ~/ First Project\n")
+    createText("#    The very first project on which i had worked, was the portfolio website which was made with only HTML-CSS-Vanilla Javascript. In this portfolio website i had worked also on UI-UX , which makes it eye soothing.")
+    createText("<a href='https://nvntdad.github.io/clg-web/' target='_blank'><i class='fab fa-wrench '></i> ~/ Second Project")
+    createText("#    The second project on which i had worked, was the college website which was made also with only HTML-CSS-Vanilla Javascript. In this college website i had intergrated an AI assitant which provides the basic details about the college. It is also made with UI-UX core principles which makes it easy to use.")
+  }
+  else if(value === "contact"){
+    trueValue(value);
+    createText("Email : nvntdadhich@gmail.com");
+   }
+   else if (value === "image"){
+    trueValue(value);
+    createText(`░░░░░░░░░░░░▒░░░░░▒▒▒▒░░░░░░░░░▒▒▒▒▒▒▒▒▒▒▒▒▒▒░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▒▒▒▒
+░░░░░░░░░░░░░░▒▒▒▒▒▒▒▒░░░░░░░░░░░▒▒░░░▒▒▒▒▒▒▒▒▒░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▒▒▒▒▒░░
+░░░░░░░░░░░░░░░▒▒▒▒▒▒▒░░░░░░░░░░░░░░░░░░░▒▒▒▒▒▒▒▒░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▒▒▒▒▒░░░░░
+░░░░░░░░░░░░░░░░▒▒▒▒▒▒░░░░░░░░░░░░░░░░▒▒▒░▒▒▒▒▒▒▒▒▒░░░░░░░░░░░░░░░░░░░░░░░░░░▒▒▒▒▒░░░░░░░░
+░░░░░░░░░░░░░░░░░░░▒▒▒▒░░░░░░░░░░░▒▒▒▒▒▓▓▓▓▓▓▓▓▒▒▒▒▒▒░░░░░░░░░░░░░░░░░░░░░▒▒▒▒░░░░░░░░░░░░
+░░░░░░░░░░░░░░░░░░▒▒░░░▒▒▒▒▒▒▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒▒░░░░░░░░░░░░░░░░▒▒▒▒░░░░░░░░░░░░░░░
+░░░░░░░░░░░░░░░░▒▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒▒▒░░░░░░░░░░▒▒▒░░░░░░░░░░░░░░░░░░░
+░░░░░░░░░░░░░▒▒▒▒▓▓▓▓▓█████████████████████████▓▓▓▓▓▓▓▓▒▒▒▒░░░░░░▒▒░░░░░░░░░░░░░░░░░░░░░░░
+░░░░░░░░░░▒░░▒▒▒▒▓▓▓▓███████████████████████████████▓▓▓▓▓▓▓▒▒▒▒▒▒▒▒░░░░░░░░░░░░░░░░░░░░░░░
+░░░░░░░░░░░░░░░▒▒▒███████████████████████████████████████▓▓▓▓▓▓▒▒▒▒░░░░░░░░░░░░░░░░░░░░░░░
+░░░░░░░░░░░░░░░▒▒▓▓███████████████████████████████████████▓▓▓▓▓▓▒▒▒▒░░░░░░░░░░░░░░░░░░░░░░
+░░░░░░░░░░░░░░░░▒▒▒▓███████████████████████████▓▓▓▓▓▓▓█▓██▓▓▓▓▓▓▓▓▒▒▒▒░░░░░░░░░░░░░░░░░░░░
+░░░░░░░▒▒▒░░░░░░▒▒▓▓██████████████████████████▓▓▓▒▒▒▒▓▓▓████▓▓▓▓▓▓▓▒▒░░░░░░░░░░░░░░░░░░░░░
+░░░░░░░░░░░░░░░░▒▒▓▓████████████████████████▓▓▓▓▓▒▒▒▓▓▓▓▓██████▓▓▓▓▓▒░░░░░░░░░░░░░░░░░░░░░
+░░░░░░░░░░░░░░░░░▒▓██████▓▓▓▓▓▓▓▓▓▓██████▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓█████▓▓▓▓▓▓▒░░░░░░░░░░░░░░░░░░░░
+░░░░░░░░░░░░░░░░░▒▓▓████▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒░░░▒▒▒▓▓▓███▓▓▓▓▓▓▓▒░░░░░░░░░░░░░░░░░░░░
+░░░░░░░░░░░░░░░░░▒██████▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▓▓▓▓▓▓▓▓▓▓▓▒░░░░░░░░░░░░░░░░░▒▓▓
+░░░░░░░░░░░░░░░░░▒██████▓▓▓▓▓██████▓▓▓▓▓▓▓▓▓▓██████████▓▓▓▓▓███▓▓▓▓▒▓▒░░░░░░░░░░░░░░░░░▓▓▒
+░░░░░░░░░░░░░░░░░░▓█████▓████████████▓▓▓▓▓▓▓██████████████▓██▓▓▓▓▓▓▓▒░░░░░░░░░░░░░░░░░▒▓▓▒
+░░░░░░░░░░░░░░░░░░▒████████████████████████████████████▓▓▓▓▓▓▓██▓▓▓▒░░░░░░░░░░░░▒▒▒░░░▒▓▓▒
+░░░░░░░░░░░░░░░░░░░▓████▓▓▓▓████████████▓▓▓██████████████▓▓▓▓▓███▓▓▒░░░░░░░▒▒▒▓▓▓▓▒░░░▓▓▒▒
+░░░░░░░░░░░░░░░░░░▓█▓▒▓▓▓▓▓████████████▒▒░░▓██████████▓██▓▓▓▓▒█▓█▓▒░░░░▒▒▓▓▒▒▒▒▓▓▓░░░▒▓▓▒▒
+░░░░░░░░░░░░░░░░░░░▒▓▒▓▓▓▓███▓▓███████▓▒▒░░░▓▓▓████▓▓▓▓▓▓▒▒▒▓▓██▓▓░░░░░▓▓▓▒▓▓▒▒▓▓▒░░░▒▓▓▓▓
+░░░░░░░░░░░░░░░░░░░░▒▓▓▓▓▓▓▓▓███████▓█▓▒▒░░░░▓▓▓▓▓▓▓▒▒▒░░░░▒▓▓▓█▓▒░░░░▒▓▓▒▒▒▒▒░▒▓░░░░▓▓▓▓▓
+░░░░░░░░░░░░░░░░░░░░░▓▒▓▓▓▓▓▓▓▓▓▓▓█▓█▓▓▓▓▒▒▒░▒▓▒▓▓▒▒▒▒▒▒▒▒▒▓▒▒▒▓░▒▒▒░░▓▒░▒░▒░▒▒▓▓░░░▒▓▓▓▓▒
+░░░░░░░░░░░░░░░░░░░░░░▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒▒▒▒▒▒░░░░▒▒▓▒▒▒▒▓▓▓▓▓░▒▓▒░░░▒░▒▒▓▒░░░░░░░░░
+░░░░░░░░░░░░░░░░░░░░░░▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓██▓▓▓███▓▒▒▒▒▒▒▒▒▒▒▓▒▒▒▒▒▓▓▒▒▓▒▓▓▒▒▒▒▒░▒▒▓▒░░░░░░░░░
+░░░░░░░░░░░░░░░░░░░░░░▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓█▓▓▓▒░░▒▒▒▒▒▓▓▒▒▒▒▒▒▓▓▓▒▒▒▓▒▒▒▒▒▒▒▒▓▓░░░░░░░░░░
+░░░░░░░░░░░░░░░░░░░░░░▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓████████▓▓▓▓▒░░▒▒▓▒▒▒▒▒▒▒▒▓▓▒▒▒▒▓▒▓▓▓▓▓▓▒▒▒░░░░░░░░░░
+░░░░░░░░░░░░░░░░░░░░░░░▓▓▓▓▓▓▓▓▓▓▓█████████████▓▓▓▓▓▓▓▒▒▒▒▒▒▒▒▒▓▒▒▒░▒▒▒▒░░░░░░░░░░░░░░░░░░
+░░░░░░░░░░░░░░░░░░░░░░░▓▓▓▓▓▓▓▓▓███████▓▓▓▓▓▓▓▓▓█████▓▒▒▒▒▒▓▓▓▓▓▒▒░░░░░░░░░░░░░░░░░░░░░░░░
+░░░░░░░░░░░░░░░░░░░░░░░▒███▓▓▓▓██████████████████████▓▓▒▒▓▓▓▓▓▓▒░░░░░░░░░░░░░░░░░░░░░░░░░░
+░░░░░░░░░░░░░░░░░░░░░░░░▓████▓███████▓▓▓███▓▓▓▓▓█████▓▓▓▓▓▓▓██▓▒░░░░░░░░░░░░░░░░░░░░░░░░░░
+░░░░░░░░░░░░░░░░░░░░░░░░░▓██████████▓▓▓▓███▓▓▓▓▓▓▓████▓▓▓▓████▓░░░░░░░░░░░░░░░░░░░░░░░░░░░
+░░░░░░░░░░░░░░░░░░░░░░░░░▒███████████████████████▓███████████▓▒░░░░░░░░░░░░░░░░░░░░░░░░░░░
+░░░░░░░░░░░░░░░░░░░░░░░░░░▒█████████████████████████████████▓▒░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+░░░░░░░░░░░░░░░░░░░░░░░░░░░▓█████████████████▓█▓▓█▓████████▓▒░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░▓████████████████████████████▓▓▒░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▓█████████████████████████▓▓░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▓██████████████████████▓▓▓▒░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▒▓▓████████████████████▓▓▓▓▒░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+░░░░░░░░░░░░░░░░░░░░░░░░░░▒▓▓▓▓▓████████████████▓▓▓▓▓▓▓▒░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+░░░░░░░░░░░░░░░░░░░▒▒▒▒▒▒▒▓▓▓▓▓▓▓███████▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+░░░░░░░░░░░░▒▒▒▒▒▒▒▒▒▒▒▒▓▓██▓▓▓▓▓▓██████▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+░░░░░▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▓▓▓▓▓▓█▓▓▓▓████▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+▒▒▒▒▒▒▒▒▒▒▒▒▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓█▓▓▓██████▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+▒▒▒▒▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓███████████▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+▒▒▒▒▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓█████████▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+▓▓▓▒▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓█████████▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒▒░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓██████▓█▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒▒░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓██████▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒░▒░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓███████▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒▒░▒░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓███████▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒▓▒░▒░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓████████▓▓▓▓▓▓▓▓▒▒▒▒▒▒▒▓░▒░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓████████▓▓▓▓▓▓▓▒▒▒▒▒▒░▓░▒░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓████████▓▓▓▓▓▒▒▒▒▒▒▒░▒▒▒░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓██▓▓▓▓▒▒▒▓▒▒▒▒▒▒▒▒▒▒▒▒░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▓▓▒░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▓▓░░░░░░░░░░░░░░░░░░░▒░░░░░░░░░░░░░░
+\n`)
+   }
+  else if(value === "skills"){
+    trueValue(value);
+    createText(" Technologies and skills i have used?")
+    createText(`| Reactjs | Nodejs | FirebaseDB | MongoDB | TailwindCss | ModuleCss | ReduxToolkit | Problem solving | HTML | CSS | Javascript | npm |\n`);
+  }
+  
+  else if(value === "clear"){
+    document.querySelectorAll("p").forEach(e => e.parentNode.removeChild(e));
+    document.querySelectorAll("section").forEach(e => e.parentNode.removeChild(e));
+    createText("write 'dank' to see the list of the command");
+  }
+  else{
+    falseValue(value);
+    createText(`Naughty Hora ke bhindi`)
+  }
+}
+
+function trueValue(value){
+  
+  const div = document.createElement("section");
+  div.setAttribute("class", "type2")
+  const i = document.createElement("i");
+  i.setAttribute("class", "fas fa-angle-right icone")
+  const mensagem = document.createElement("h2");
+  mensagem.setAttribute("class", "sucess")
+  mensagem.textContent = `${value}`;
+  div.appendChild(i);
+  div.appendChild(mensagem);
+  app.appendChild(div);
+}
+
+function falseValue(value){
+  
+  const div = document.createElement("section");
+  div.setAttribute("class", "type2")
+  const i = document.createElement("i");
+  i.setAttribute("class", "fas fa-angle-right icone error")
+  const mensagem = document.createElement("h2");
+  mensagem.setAttribute("class", "error")
+  mensagem.textContent = `${value}`;
+  div.appendChild(i);
+  div.appendChild(mensagem);
+  app.appendChild(div);
+}
+
+function createText(text, classname){
+  const p = document.createElement("p");
+  
+  p.innerHTML =
+  text
+  ;
+  app.appendChild(p);
+}
+
+function createCode(code, text){
+  const p = document.createElement("p");
+  p.setAttribute("class", "code");
+  p.innerHTML =
+ `${code} <br/><span class='text'> ${text} </span>`;
+  app.appendChild(p);
+}
+
+open_terminal();
